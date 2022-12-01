cf75260d3ca1bec4a3ace7eb0f753bb235a1f99d
frn-dawn <frn@dawnhealth.com>
2022-12-01 15:23:09 +0100

    Find top 3 sum

--- 1/count-calories.ts
+++ 1/count-calories.ts
@@ -1,5 +1,10 @@
 import chalk from 'chalk';
 import fs from 'fs';
+import { cloneDeep } from 'lodash';
+interface Collection {
+  sum: number;
+  allNumbers: number[];
+}
 
 fs.readFile(__dirname + '/data.txt', (err, data) => {
   if (err) {
@@ -8,7 +13,7 @@ fs.readFile(__dirname + '/data.txt', (err, data) => {
   }
 
   if (data) {
-    const collections = data
+    const collections: Collection[] = data
       .toString()
       .split('\n\n') // split on groups (divided by new line)
       .map((collectionTxt) => {
@@ -22,45 +27,111 @@ fs.readFile(__dirname + '/data.txt', (err, data) => {
         return { sum, allNumbers };
       });
 
-    const largest = collections
-      .map((collection) => collection.sum)
-      .reduce((accumulatedValue, currentValue) =>
-        Math.max(accumulatedValue, currentValue)
+    try {
+      const largest = getHighestSum(collections);
+      const topThreeSum = getTopThreeSum(collections);
+
+      // Print results
+      console.log(
+        chalk.bgBlueBright.underline(`\nLargest number: ${largest}\n`)
       );
-
-    // Print all values, highlight the largest value
-    collections.forEach((collection, index) => {
-      if (collection.sum > largest) {
-        console.error(
-          chalk.bgRedBright('\nWut, is the value higher than largest value??\n')
-        );
-        process.exit(1);
-      }
-
-      if (collection.sum === largest) {
-        console.log(
-          chalk.bgGreen(
-            `${index}) Sum: ${collection.sum} - ${JSON.stringify(
-              collection.allNumbers
-            )}`
-          )
-        );
-      } else {
-        console.log(
-          chalk.bgBlue(
-            `${index}) Sum: ${collection.sum} - ${JSON.stringify(
-              collection.allNumbers
-            )}`
-          )
-        );
-      }
-    });
-
-    // Print result (largest)
-    console.log(chalk.bgGreen.underline(`\nLargest number: ${largest}\n`));
+      console.log(
+        chalk.bgBlueBright.underline(`\nTop three sum: ${topThreeSum}\n`)
+      );
+    } catch (error) {
+      console.error(chalk.bgRedBright(error));
+      process.exit(1);
+    }
 
     process.exit(0);
   }
 
   return data;
 });
+
+const getHighestSum = (collections: Collection[]): number => {
+  const largest = collections
+    .map((collection) => collection.sum)
+    .reduce((accumulatedValue, currentValue) =>
+      Math.max(accumulatedValue, currentValue)
+    );
+
+  console.log(
+    chalk.bgBlue(`
+----------------------------------------------
+Printing all sums, highlighting largest number
+----------------------------------------------
+`)
+  );
+
+  // Print all values, highlight the largest value
+  collections.forEach((collection, index) => {
+    if (collection.sum > largest) {
+      throw new Error('\nWut, this value is higher than the largest value.\n');
+    }
+
+    // highlight largest sum
+    if (collection.sum === largest) {
+      console.log(
+        chalk.bgGreen(
+          `${index}) Sum: ${collection.sum} - ${JSON.stringify(
+            collection.allNumbers
+          )}`
+        )
+      );
+    } else {
+      console.log(
+        `${index}) Sum: ${collection.sum} - ${JSON.stringify(
+          collection.allNumbers
+        )}`
+      );
+    }
+  });
+
+  // Print result (largest)
+  // console.log(chalk.bgGreen.underline(`\nLargest number: ${largest}\n`));
+
+  return largest;
+};
+
+const getTopThreeSum = (collections: Collection[]) => {
+  // Sort all collections with highest sum at the top
+  const sortedCollections = cloneDeep(collections).sort(
+    (a, b) => b.sum - a.sum
+  );
+
+  console.log(
+    chalk.bgBlue(`
+------------------------------------------------
+Printing ordered collections, highlighting top 3
+------------------------------------------------
+`)
+  );
+
+  // Print all values, highlight top 3
+  sortedCollections.forEach((collection, index) => {
+    // highlight top 3 items
+    if (index < 3) {
+      console.log(
+        chalk.bgGreen(
+          `${index}) Sum: ${collection.sum} - ${JSON.stringify(
+            collection.allNumbers
+          )}`
+        )
+      );
+    } else {
+      console.log(
+        `${index}) Sum: ${collection.sum} - ${JSON.stringify(
+          collection.allNumbers
+        )}`
+      );
+    }
+  });
+
+  const topThree = sortedCollections.slice(0, 3);
+
+  if (topThree.length !== 3)
+    throw new Error(`Top three must have 3 items, not ${topThree.length}`);
+
+  return topThree.reduce((a, b) => a + b.sum, 0);
+};
